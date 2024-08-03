67f69acf005b7cd0c0e5d3a948eb0b0bbd5638ba
Christian Abila <christian@abila.at>
2024-08-03 12:29:34 +0200

    correct language strings order

--- lang/en/tool_clearbackupfiles.php
+++ lang/en/tool_clearbackupfiles.php
@@ -21,21 +21,27 @@
  * @copyright  2015 Shubhendra Doiphode
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-$string['pluginname'] = 'Clear backup files';
-$string['coursebackupremoved'] = 'Course backup deleted';
-$string['backupremovedlog'] = 'Course backup file {$a->filename} of size {$a->filesize} was deleted.';
-$string['clearbackupcompleted'] = 'Clear backup completed';
-$string['backupcompletedlog'] = 'During this operation {$a->filecount} course backup files of total size {$a->filesize} were deleted.';
-$string['filename'] = 'File name';
-$string['filesize'] = 'File size';
-$string['filedeletedheader'] = 'The course backup files deleted during this operation are as follows:';
-$string['filedeletedfooter'] = 'In total {$a->filecount} backup files were deleted and {$a->filesize} of server space was cleared.';
-$string['filedeletedempty'] = 'There are no backup files to delete.';
 
-$string['warningmsg'] = 'Please note, clearing of backup files is an irreversible process, you will not be able to restore deleted backup files.';
-$string['warningalert'] = 'Are you sure you want to continue?';
+$string['backupcompletedlog'] = 'During this operation {$a->filecount} course backup files of total size {$a->filesize} were deleted.';
+$string['backupremovedlog'] = 'Course backup file {$a->filename} of size {$a->filesize} was deleted.';
+
+$string['clearbackupcompleted'] = 'Clear backup completed';
+$string['continuetoclearbackup'] = 'Continue to clear backup';
+$string['coursebackupremoved'] = 'Course backup deleted';
+
 $string['days'] = 'Days';
 $string['daysdesc'] = 'Number of days of backup to clear';
-$string['continuetoclearbackup'] = 'Continue to clear backup';
+
 $string['enablecron'] = 'Enable CRON';
 $string['enablecrondesc'] = '';
+
+ $string['filedeletedempty'] = 'There are no backup files to delete.';
+ $string['filedeletedfooter'] = 'In total {$a->filecount} backup files were deleted and {$a->filesize} of server space was cleared.';
+ $string['filedeletedheader'] = 'The course backup files deleted during this operation are as follows:';
+ $string['filename'] = 'File name';
+ $string['filesize'] = 'File size';
+
+$string['pluginname'] = 'Clear backup files';
+
+$string['warningalert'] = 'Are you sure you want to continue?';
+$string['warningmsg'] = 'Please note, clearing of backup files is an irreversible process, you will not be able to restore deleted backup files.';
