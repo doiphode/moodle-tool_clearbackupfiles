4be9555911d8011d5c200366a15fc50b97cf3e31
S Doiphode <doiphode.sunny@gmail.com>
2025-01-29 13:39:50 +0000

    Version bump

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024080600;
+$plugin->version   = 2025012900;
 $plugin->requires  = 2016110800;
 $plugin->component = 'tool_clearbackupfiles';
 $plugin->release = '4.1, release 3';
